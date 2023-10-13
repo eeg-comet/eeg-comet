039ec0d9d718393373b2fb9cb64b94eaef33e8ad
aminkabir <kabir@sfu.ca>
2023-10-13 10:24:01 -0700

    Update newstudywindow.py

--- src/main/python/microstate_tool/gui/newstudywindow.py
+++ src/main/python/microstate_tool/gui/newstudywindow.py
@@ -392,7 +392,7 @@ class NewStudyWindow(QDialog):
 
         if self.main_window:
             self.main_window.tbx = self.tbx
-            self.main_window.load_study(self.save_dir)
+            self.main_window.load_study()
         self.ui.close()
 
     def plot_montage(self):
