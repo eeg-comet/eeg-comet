8ff56568de1ffe2daccd2f430f3386c42f8c1eb1
aminkabir <kabir@sfu.ca>
2023-12-16 15:53:44 -0800

    Update feature_io.py

--- EEG_COMET/functions/features_utils/feature_io.py
+++ EEG_COMET/functions/features_utils/feature_io.py
@@ -1,22 +1,17 @@
 """
 This class provides methods for exporting and importing calculated features_utils to and from files.
-It supports exporting features_utils in both 'static' and 'dynamic' modes, and importing features_utils from various file formats.
+
 """
 
 import os.path
 import pandas as pd
+
 
 class FeatureIO:
     @staticmethod
     def export_features(features_df, mode, output_folder, export_format='.csv'):
         """
         Export calculated features to the specified file format.
-
-        Args:
-            features_df (DataFrame): DataFrame containing extracted features.
-            mode (str): Feature extraction mode ('static' or 'dynamic').
-            output_folder (str): Path to the output folder.
-            export_format (str): Format for exporting ('.csv', '.pkl', '.hdf', or '.json').
         """
         output_path = os.path.join(output_folder, f'{mode}_features{export_format}')
 
@@ -35,13 +30,6 @@ class FeatureIO:
     def import_features(file_path, import_format='.csv'):
         """
         Import calculated features from a file.
-
-        Args:
-            file_path (str): Path to the input file.
-            import_format (str): Format of the imported file ('.csv', '.pkl', '.hdf', or '.json').
-
-        Returns:
-            DataFrame: Imported features.
         """
         if import_format == '.csv':
             features = pd.read_csv(file_path)
