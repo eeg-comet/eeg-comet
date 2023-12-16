fac1f66fa6b6769c602c66d3c6e67761fe820fdf
aminkabir <kabir@sfu.ca>
2023-12-16 14:21:41 -0800

    Update data_io.py

--- EEG_COMET/functions/data_utils/data_io.py
+++ EEG_COMET/functions/data_utils/data_io.py
@@ -1,7 +1,7 @@
 """
 Description: EEG Data Loading and Preprocessing Script
 
-This script provides functions for loading and preprocessing EEG data from different formats using the MNE-Python library.
+This script provides functions for loading and preprocessing EEG data from different formats using the MNE library.
 It supports loading raw or epoched data, applying channel location information, removing channels, and applying average
 reference projection. The script is designed to enhance flexibility and efficiency when working with EEG data.
 """
@@ -12,54 +12,34 @@ from fnmatch import fnmatch
 import numpy as np
 import mne
 
+
 class DataIO:
     def __init__(self):
         pass
-    
-    def save_eeg_info(self, eeg_info_path, eeg_info):
+
+    @staticmethod
+    def save_eeg_info(eeg_info_path, eeg_info):
         """
         Save EEG information to a binary file using pickle.
-    
-        Inputs:
-        - eeg_info_path (str): File path for saving EEG information.
-        - eeg_info (mne.Info): EEG information obtained from raw.info or epoched.info.
-    
-        Outputs:
-        None
         """
     
         with open(eeg_info_path, 'wb') as f:
             pickle.dump(eeg_info, f)  # Serialize and save the eeg_info object to the specified file path
-    
-    
-    def load_eeg_info(self, eeg_info_path):
+
+    @staticmethod
+    def load_eeg_info(eeg_info_path):
         """
         Load EEG information from a binary file using pickle.
-    
-        Inputs:
-        - eeg_info_path (str): File path to load EEG information.
-    
-        Outputs:
-        - eeg_info (mne.Info): Loaded EEG information from the saved pickle file.
         """
     
         with open(eeg_info_path, 'rb') as f:
             eeg_info = pickle.load(f)  # Deserialize and load the eeg_info object from the specified file path
         return eeg_info
-    
-    
-    def find_data(self, input_folder, extension, pattern='*'):
+
+    @staticmethod
+    def find_data(input_folder, extension, pattern='*'):
         """
         Recursively search for data files within a folder based on extension and pattern.
-    
-        Inputs:
-        - input_folder (str): Folder path where data files are located.
-        - extension (str): File extension to search for (e.g., ".hdf").
-        - pattern (str): Pattern to match in file names (e.g., "eyes_closed").
-    
-        Outputs:
-        - list_path (list): List of file paths matching the search criteria.
-        - list_filename (list): List of file names matching the search criteria.
         """
     
         list_path = []  # Initialize an empty list to store matching file paths
@@ -74,23 +54,12 @@ class DataIO:
                     list_path.append(os.path.join(path, name))
                     list_filename.append(name.split('.')[0])
         return list_path, list_filename
-    
-    
-    def load_eegs(self, filename, eeg_format, datatype, channel_location_dir='', chan2rm=[]):
+
+    @staticmethod
+    def load_eegs(filename, eeg_format, datatype, channel_location_dir='', chan2rm=[]):
         """
         Load EEG data from different formats and preprocess if needed.
-    
-        Inputs:
-        - filename (str): Path to the EEG data file.
-        - eeg_format (str): Format of the EEG data file (e.g., '.edf', '.fif', '.set').
-        - datatype (str): Type of EEG data ('raw' for continuous data or 'epoched' for segmented data).
-        - channel_location_dir (str): Directory path for channel location information (optional).
-        - chan2rm (list): List of channel names to be removed (optional).
-    
-        Outputs:
-        - eeg_data (mne.Raw or mne.Epochs): Loaded and optionally preprocessed EEG data.
         """
-
         verbose = 'ERROR'
 
         if datatype == 'raw':
@@ -144,20 +113,11 @@ class DataIO:
             eeg = eeg.drop_channels('TRIGGER')
     
         return eeg
-    
-    
-    def export_eegs(self, eeg, save_path, extension, datatype):
+
+    @staticmethod
+    def export_eegs(eeg, save_path, extension, datatype):
         """
         Export EEG data to a specified file format.
-    
-        Inputs:
-        - eeg (mne.Raw or mne.Epochs): EEG data to be exported (raw or epoched).
-        - save_path (str): Directory path to save the exported EEG data file.
-        - extension (str): Desired file extension (e.g., '.vhdr', '.set', '.edf').
-        - datatype (str): Type of EEG data ('raw' or 'epoched').
-    
-        Output:
-        - None
         """
 
         # List of available file extensions for export
@@ -172,18 +132,11 @@ class DataIO:
             mne.export.export_raw(save_path + extension, eeg, fmt='auto', overwrite=True)
         elif datatype == 'epoched':
             mne.export.export_epochs(save_path + extension, eeg, fmt='auto', overwrite=True)
-    
-    
-    def get_eeg_data(self, eeg, datatype):
+
+    @staticmethod
+    def get_eeg_data(eeg, datatype):
         """
         Extract EEG data from MNE-Python Epochs or Raw object.
-    
-        Inputs:
-        - eeg (mne.Epochs or mne.Raw): EEG data (epochs or continuous raw data).
-        - datatype (str): EEG data type ('epoched' for segmented data, otherwise 'raw').
-    
-        Output:
-        - eeg_data (numpy.ndarray): Extracted EEG data as a NumPy array.
         """
     
         if datatype == "epoched":
@@ -198,5 +151,4 @@ class DataIO:
             eeg_data = eeg.get_data()  # Get continuous raw data
     
         return eeg_data
-    
     
