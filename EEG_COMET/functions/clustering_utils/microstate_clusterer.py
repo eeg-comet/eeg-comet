1066fc6d45021075a1aacae802416e0bea0d0426
aminkabir <kabir@sfu.ca>
2023-12-16 16:08:46 -0800

    Update microstate_clusterer.py

--- EEG_COMET/functions/clustering_utils/microstate_clusterer.py
+++ EEG_COMET/functions/clustering_utils/microstate_clusterer.py
@@ -6,14 +6,11 @@ Clustering Functions
 """
 
 import numpy as np
-from tqdm import tqdm
 from scipy import spatial
-from sklearn.model_selection import KFold
-from pyclustering.cluster import kmeans, xmeans, agglomerative, elbow, silhouette
-from pyclustering.cluster.agglomerative import type_link
+from pyclustering.cluster import kmeans, xmeans
 from pyclustering.utils.metric import distance_metric, type_metric
 from functions.data_utils.extract_peaks_maps import initialize_cluster_centers, generate_maps_and_peaks
-from keras.layers import Conv1D, Flatten, Dense, Reshape, Input
+from keras.layers import Flatten, Dense, Reshape, Input
 from keras.models import Model
 from sklearn.decomposition import PCA
 from joblib import Parallel, delayed
@@ -30,42 +27,41 @@ class MicrostateClusterer:
         self.best_gev = 0
         self.best_residual = None
 
-    def corr_vectors(self, A, B, axis=0):
-        """Computes the Pearson correlation between two matrices A and B along a specified axis."""
-
+    @staticmethod
+    def corr_vectors(array1, array2, axis=0):
+        """
+        Computes the Pearson correlation between two matrices A and B along a specified axis.
+        """
         # Center and normalize matrices
-        An = A - np.mean(A, axis=axis, keepdims=True)
-        An /= np.linalg.norm(An, axis=axis, ord=2, keepdims=True)
-        Bn = B - np.mean(B, axis=axis, keepdims=True)
-        Bn /= np.linalg.norm(Bn, axis=axis, ord=2, keepdims=True)
-
-        return np.sum(An * Bn, axis=axis)
+        array1n = array1 - np.mean(array1, axis=axis, keepdims=True)
+        array1n /= np.linalg.norm(array1n, axis=axis, ord=2, keepdims=True)
+        array2n = array2 - np.mean(array2, axis=axis, keepdims=True)
+        array2n /= np.linalg.norm(array2n, axis=axis, ord=2, keepdims=True)
+        return np.sum(array1n * array2n, axis=axis)
 
     def compute_gev(self, data, maps):
-        """Calculates the global explained variance (GEV) of microstate maps based on input data."""
-
+        """
+        Calculates the global explained variance (GEV) of microstate maps based on input data.
+        """
         gfp = np.std(data, axis=0)  # Global Field Power
-
         # Normalize maps
         if maps.ndim == 1:
             maps = maps / np.linalg.norm(maps)
             maps = np.reshape(maps, (1, -1))
         else:
             maps = maps / np.linalg.norm(maps, axis=1, keepdims=True)
-
         # Compute activation and segmentation
         activation = maps.dot(data)
         segmentation = np.argmax(np.abs(activation), axis=0)
-
         selected_maps = maps[segmentation, :]
         map_corr = self.corr_vectors(data, selected_maps.T)
-
         return np.sum((gfp * map_corr) ** 2) / np.sum(gfp ** 2)
 
     @staticmethod
     def calculate_spatial_similarity(metric, point1, point2):
-        """Computes similarity between two points using cosine similarity or spatial correlation."""
-
+        """
+        Computes similarity between two points using cosine similarity or spatial correlation.
+        """
         if metric == 'Cosine Similarity':
             # Calculates the cosine similarity
             dist = spatial.distance.cosine(point1, point2)
@@ -76,86 +72,75 @@ class MicrostateClusterer:
             raise ValueError("Failed to match metric")
         return 1 - dist
 
-    def modified_kmeans(self, data, initial_maps, n_states, max_iter=500, thresh=1e-6, verbose=True):
-        """Performs modified K-Means clustering on data with a specified number of microstate maps."""
+    @staticmethod
+    def modified_kmeans(data, initial_maps, n_states, max_iter=500, thresh=1e-6, verbose=True):
+        """
+        Performs modified K-Means clustering on data with a specified number of microstate maps.
+        """
         # Initial setup
         n_channels, n_samples = data.shape
         maps = initial_maps.copy()
         data_sum_sq = np.sum(data ** 2)
         prev_residual = np.inf
-
         # Clustering iterations
         for iteration in range(max_iter):
             # Assign each sample to the best matching microstate
             activation = maps.dot(data)
             segmentation = np.argmax(np.abs(activation), axis=0)
-
             for state in range(n_states):
                 idx = (segmentation == state)
                 maps[state] = np.dot(data[:, idx], activation[state, idx])
                 maps[state] /= np.linalg.norm(maps[state])
-
             # Estimate residual noise
             act_sum_sq = np.sum(np.sum(maps[segmentation].T * data, axis=0) ** 2)
             residual = abs(data_sum_sq - act_sum_sq) / float(n_samples * (n_channels - 1))
-
             # Check for convergence
             if (prev_residual - residual) < (thresh * residual):
                 if verbose:
                     print('Converged at', iteration, 'iterations.')
                 break
-
             prev_residual = residual
-
         return maps, prev_residual
 
     def run_modified_kmeans(self, preprocessed_data_path, extension, datatype,
                             maps2use, n_states, n_inits, initializer='Random', max_iter=500, thresh=1e-6, verbose=True):
-        """Runs modified K-Means clustering with multiple initializations to find the best microstate maps."""
-
+        """
+        Runs modified K-Means clustering with multiple initializations to find the best microstate maps.
+        """
         all_data, _ = generate_maps_and_peaks(preprocessed_data_path, extension, datatype, use_percentages=100)
         best_residual, best_gev, best_maps = None, 0, None
         modified_kmeans_results = {}
-
         for init in range(n_inits):
             if verbose:
                 print(f'\nClustering #{init + 1} of {n_inits}')
-
             initial_maps = initialize_cluster_centers(maps2use, n_states, initializer)
             maps, residual = self.modified_kmeans(maps2use, initial_maps, n_states, max_iter, thresh, verbose=verbose)
             gev = self.compute_gev(all_data, maps)
-
             # Store the results for this initialization in the dictionary
             modified_kmeans_results[init] = {
                 'maps': maps,
                 'gev': gev,
                 'residual': residual
             }
-
             if verbose:
                 print(f'Found {n_states} Microstate Maps')
                 print(f'GEV: {gev}')
-
             # Update the best results if current gev is higher
             if gev > best_gev:
                 best_residual, best_gev, best_maps = residual, gev, maps
-
         modified_kmeans_results['best'] = {
             'maps': best_maps,
             'gev': best_gev,
             'residual': best_residual
         }
-
         if verbose:
             print(f'\nBest GEV: {best_gev}')
         return modified_kmeans_results
 
-
-
     def run_aahc(self, preprocessed_data_path, extension, datatype, maps2use, n_states, n_maps2use=1000, verbose=True):
-        """Performs AAHC clustering on data with a specified number of microstate maps."""
-
-
+        """
+        Performs AAHC clustering on data with a specified number of microstate maps.
+        """
         def select_random_maps_subset(maps2use, n_maps2use):
             # Generate random indices to select maps
             random_indices = np.random.choice(maps2use.shape[1], size=n_maps2use, replace=False)
@@ -181,11 +166,9 @@ class MicrostateClusterer:
             principal_component = eigenvectors[:, np.argmax(np.abs(eigenvalues))]
             principal_component = np.real(principal_component)
             reconstructed_data = principal_component / np.sqrt(np.sum(principal_component ** 2))
-
             # Calculate residual (difference between original and reconstructed data)
             residual = cluster_data_subset - np.dot(reconstructed_data, cluster_data_subset.T).T
             maps[cluster_index, :] = principal_component / np.sqrt(np.sum(principal_component ** 2))
-
             return residual
 
         # Initial setup
@@ -203,14 +186,12 @@ class MicrostateClusterer:
         batch_size = int(n_maps/10)
         cluster_data = maps
         print(f"Initial number of clusters: {n_maps:d}\n")
-
         # Cluster indices w.r.t. original size, normalized GFP peak data
         Ci = [[k] for k in range(n_maps)]
-
         # Main loop: atomize + agglomerate
         while (n_maps > n_states):
-            print(f"\r\r\t\tAAHC > n: {n_maps:d} => {n_maps - 1:d}", end="")
-
+            if verbose:
+                print(f"\r\r\t\tAAHC > n: {n_maps:d} => {n_maps - 1:d}", end="")
             # Correlations of the data sequence with each cluster
             # Assuming you initialize 'segmentation' somewhere before the loop
             segmentation = np.zeros(n_samples, dtype=int)
@@ -222,7 +203,6 @@ class MicrostateClusterer:
             for i in range(0, n_samples, batch_size):
                 data_batch = all_data[:, i:i + batch_size]
                 activation[:, i:i + batch_size] = maps.dot(data_batch)
-
             # GEV (global explained variance) of cluster k
             gev = np.zeros(n_maps)
             for state in range(n_maps):
@@ -231,51 +211,44 @@ class MicrostateClusterer:
                 gev[state] = np.sum((gfp[idx] * map_corr) ** 2) / gfp2
             # Merge cluster with the minimum GEV
             imin = np.argmin(gev)
-
             # N => N-1
             maps = np.vstack((maps[:imin, :], maps[imin + 1:, :]))
             Ci, reC = Ci[:imin] + Ci[imin + 1:], Ci[imin]
-
             re_cluster = []  # indices of updated clusters
             # Parallelize the loop
             Parallel(n_jobs=n_jobs)(delayed(process_reassignment)(cluster_index_to_reassign, Ci, cluster_data, maps) for
                                     cluster_index_to_reassign in reC)
             n_maps = len(Ci)
-
             # Update clusters
             re_cluster = list(set(re_cluster))  # unique list of updated clusters
-
             # Parallelize the loop
             residuals = Parallel(n_jobs=n_jobs)(
                 delayed(process_cluster)(cluster_index, Ci, cluster_data, maps) for cluster_index in re_cluster)
-
         best_gev = self.compute_gev(all_data, maps)
         print(f'\nBest GEV: {best_gev}')
         return maps, np.sum(residuals), best_gev
 
-
-    def create_eeg_autoencoder(self, input_shape, encoding_dim):
-        """Creates an autoencoder model for EEG data compression and reconstruction."""
+    @staticmethod
+    def create_eeg_autoencoder(input_shape, encoding_dim):
+        """
+        Creates an autoencoder model for EEG data compression and reconstruction.
+        """
         # Encoder
         input_layer = Input(shape=input_shape, name='input')
         x = Flatten()(input_layer)
         encoded = Dense(encoding_dim, activation='relu', name='embedding')(x)
-
         # Decoder
         x = Dense(np.prod(input_shape), activation='relu')(encoded)
         decoded = Reshape(input_shape)(x)
-
         autoencoder = Model(input_layer, decoded, name='autoencoder')
         encoder = Model(input_layer, encoded, name='encoder')
-
         # Compile the autoencoder (you can change the optimizer and loss function if needed)
         autoencoder.compile(optimizer='adam', loss='mse')
-
         return autoencoder, encoder
 
-    def find_original_centroids(self, eeg_data, cluster_labels, n_clusters):
+    @staticmethod
+    def find_original_centroids(eeg_data, cluster_labels, n_clusters):
         """Determines the original centroids of clustered data points."""
-
         original_centroids = []
         for cluster_id in range(n_clusters):
             cluster_indices = np.where(cluster_labels == cluster_id)[0]
@@ -283,16 +256,14 @@ class MicrostateClusterer:
             cluster_mean = np.mean(cluster_data, axis=0)
             original_centroids.append(cluster_mean)
         original_centroids = np.array(original_centroids)
-
         return original_centroids
 
-    def calculate_residuals(self, eeg_data, autoencoder):
+    @staticmethod
+    def calculate_residuals(eeg_data, autoencoder):
         # Encode and then decode the data to get the reconstructed data
         reconstructed_data = autoencoder.predict(eeg_data)
-
         # Calculate residuals (reconstruction errors) for each data point
         residuals = np.mean(np.abs(eeg_data - reconstructed_data), axis=1)
-
         return residuals
 
     def extract_features_with_autoencoder(self, eeg_data, encoding_dim=10):
@@ -300,45 +271,26 @@ class MicrostateClusterer:
         # Create the autoencoder
         input_shape = eeg_data.shape[1:]
         autoencoder, encoder = self.create_eeg_autoencoder(input_shape, encoding_dim)
-
         # Train the autoencoder on EEG data
         autoencoder.fit(eeg_data, eeg_data, epochs=10, batch_size=64, shuffle=True)
-
         # Extract features using the encoder
         encoded_features = encoder.predict(eeg_data)
-
         return encoded_features, autoencoder
 
-    def extract_features_with_pca(self, eeg_data, pca_components=10):
-        """Reduces dimensionality of EEG data using Principal Component Analysis (PCA)."""
+    @staticmethod
+    def extract_features_with_pca(eeg_data, pca_components=10):
+        """
+        Reduces dimensionality of EEG data using Principal Component Analysis (PCA).
+        """
         # Perform PCA to reduce dimensionality
         pca = PCA(n_components=pca_components)
         reduced_data = pca.fit_transform(eeg_data)
-
         return reduced_data
 
     def clustering_func(self, preprocessed_data_path, extension, datatype, n_pca, method, n_states, initializer,
                         use_percentages, min_dist, clustering_option):
         """
         Performs clustering on preprocessed data to find microstate maps.
-
-        Parameters:
-            preprocessed_data_path: Path to the preprocessed data.
-            hdf_concatenated_data_path: Path to the HDF concatenated data
-            n_pca: Number of principal components for the PCA method
-            method: Clustering method to use
-            n_states: Number of microstate maps
-            initializer: Initialization method
-            min_dist: Minimum distance
-            n_inits: Number of initializations
-            tolerance: Convergence threshold
-            clustering_option: Distance metric to use for clustering
-
-        Returns:
-            best_maps: Best cluster centers (microstate maps)
-            best_gev: Best global explained variance
-            best_residual: Best residual error
-            n_states: Optimal number of clusters (microstate maps)
         """
 
         # Create an instance of the progress dialog
@@ -379,7 +331,6 @@ class MicrostateClusterer:
             )
 
         else:
-
             initial_centers = initialize_cluster_centers(maps2use, n_states, initializer)
             maps2use = np.transpose(maps2use)
 
@@ -431,7 +382,6 @@ class MicrostateClusterer:
             else:
                 raise ValueError("Failed to match method")
 
-
             best_gev = 0
             for init in range(self.number_of_repeats):
                 progress_dialog.set_label_text(f"Clustering {int(n_states)} Microstate Maps\nInitialization #{init + 1} of {self.number_of_repeats}")
@@ -453,19 +403,17 @@ class MicrostateClusterer:
                     # Calculate residuals
                     residual = 0#self.calculate_residuals(maps2use, autoencoder)
 
-                GEV_R = self.compute_gev(np.transpose(maps2use), np.array(centroids))
+                gev_r = self.compute_gev(np.transpose(maps2use), np.array(centroids))
 
                 progress_dialog.update_progress(init + 1, self.number_of_repeats)
-                progress_dialog.set_line_edit_text(f"Global Explained Variance: {GEV_R}")
+                progress_dialog.set_line_edit_text(f"Global Explained Variance: {gev_r}")
                 print('Found', str(int(n_states)), 'Microstate Maps')
-                print('GEV:', str(GEV_R))
-                if GEV_R > best_gev:
-                    best_gev = GEV_R
+                print('GEV:', str(gev_r))
+                if gev_r > best_gev:
+                    best_gev = gev_r
                     best_maps = centroids
                     best_residual = residual
-
             print('\nBest GEV:', str(best_gev))
             progress_dialog.close()
 
         return best_maps, best_gev, best_residual
-
