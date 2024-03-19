fa45581c006e30a234adae1b336fc15260167863
aminkabir <kabir@sfu.ca>
2024-03-19 11:00:59 -0700

    Update feature_io.py

--- EEG_COMET/functions/features_utils/feature_io.py
+++ EEG_COMET/functions/features_utils/feature_io.py
@@ -1,18 +1,26 @@
-"""
-This class provides methods for exporting and importing calculated features_utils to and from files.
-
-"""
 
 import os.path
 import pandas as pd
 
 
 class FeatureIO:
+    """
+    The FeatureIO class provides methods for exporting and importing calculated features.
+    """
+
     @staticmethod
     def export_features(features_df, feature_type, feature_mode, output_folder, export_format='.csv'):
         """
         Export calculated features to the specified file format.
+
+        Args:
+            features_df (pandas.DataFrame): The DataFrame containing the calculated features.
+            feature_type (str): The type of the features.
+            feature_mode (str): The mode of the features.
+            output_folder (str): The folder path to save the exported file.
+            export_format (str, optional): The format of the exported file. Defaults to '.csv'.
         """
+
         output_path = os.path.join(output_folder, f'{feature_type}_{feature_mode}_features{export_format}')
 
         if export_format == '.csv':
@@ -30,7 +38,15 @@ class FeatureIO:
     def import_features(file_path, import_format='.csv'):
         """
         Import calculated features from a file.
+
+        Args:
+            file_path (str): The path to the file containing the calculated features.
+            import_format (str, optional): The format of the imported file. Defaults to '.csv'.
+
+        Returns:
+            pandas.DataFrame: The imported DataFrame containing the calculated features.
         """
+
         if import_format == '.csv':
             features = pd.read_csv(file_path)
         elif import_format == '.pkl':
@@ -41,5 +57,4 @@ class FeatureIO:
             features = pd.read_json(file_path, orient='records', lines=True)
         else:
             raise ValueError("Invalid import format.")
-
         return features
