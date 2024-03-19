1eb62b95263a39118b7f2ef7ee684245da521a77
aminkabir <kabir@sfu.ca>
2024-03-18 23:54:02 -0700

    Update data_initializer.py

--- EEG_COMET/functions/data_utils/data_initializer.py
+++ EEG_COMET/functions/data_utils/data_initializer.py
@@ -1,8 +1,3 @@
-"""
-Description: This script provides functions for extracting GFP (Global Field Power) peaks,
-and generating microstate maps for clustering_utils.
-
-"""
 
 import numpy as np
 from scipy.signal import find_peaks
@@ -11,6 +6,9 @@ from functions.data_utils.data_io import DataIO
 
 
 class DataInitializer:
+    """
+    The DataInitializer class provides methods for initializing data for microstate analysis.
+    """
     def __init__(self):
         pass
 
@@ -18,16 +16,29 @@ class DataInitializer:
     def initialize_cluster_centers(maps2use, n_states, initializer):
         """
         Initialize cluster centers for k-means clustering algorithm.
+
+        Args:
+            maps2use (numpy.ndarray): Array containing the microstate maps.
+            n_states (int): Number of microstate states.
+            initializer (str): Initialization method ('K-Means++' or 'Random').
+
+        Returns:
+            numpy.ndarray: Initialized cluster centers.
         """
+
         if initializer == 'K-Means++':
-            initial_centers = []
             initial_idx = np.random.choice(np.size(maps2use, 1))
-            initial_centers.append(maps2use[:, initial_idx])
+            initial_centers = [maps2use[:, initial_idx]]
             for _ in range(1, n_states):
-                dists = np.array([
-                    min([abs(correlate(d, c, mode='valid')[0]) for c in initial_centers])
-                    for d in maps2use.T
-                ])
+                dists = np.array(
+                    [
+                        min(
+                            abs(correlate(d, c, mode='valid')[0])
+                            for c in initial_centers
+                        )
+                        for d in maps2use.T
+                    ]
+                )
                 probs = dists / dists.sum()
                 next_idx = np.random.choice(np.size(maps2use, 1), p=probs)
                 next_centroid = maps2use[:, next_idx]
@@ -47,19 +58,25 @@ class DataInitializer:
     def extract_gfp_peaks_and_maps(data, use_percentages=None, min_dist=None):
         """
         Extract GFP peaks and maps at peaks from EEG data.
+
+        Args:
+            data (numpy.ndarray): Array containing the EEG data.
+            use_percentages (float, optional): Percentage of data to use for peak extraction. Defaults to None.
+            min_dist (int, optional): Minimum distance between peaks. Defaults to None.
+
+        Returns:
+            tuple: A tuple containing the extracted maps and peak indices.
         """
         gfp = np.std(data, axis=0)
 
         if use_percentages is not None:
             num_samples = int(data.shape[1] * (int(use_percentages) / 100))
             peaks = np.random.choice(data.shape[1], size=num_samples, replace=False)
-            maps = data[:, peaks]
         else:
             if min_dist == 0:
                 min_dist = None
             peaks, _ = find_peaks(gfp, distance=min_dist)
-            maps = data[:, peaks]
-
+        maps = data[:, peaks]
         maps /= np.linalg.norm(maps, axis=1, keepdims=True)
         return maps, peaks
 
@@ -67,6 +84,16 @@ class DataInitializer:
     def generate_maps_and_peaks(preprocessed_folder, extension, datatype, use_percentages=None, min_dist=None):
         """
         Generate GFP maps and peak indices from preprocessed EEG data.
+
+        Args:
+            preprocessed_folder (str): Path to the preprocessed EEG data folder.
+            extension (str): File extension of the preprocessed EEG data files.
+            datatype (str): Data type ('continuous' or 'epoched').
+            use_percentages (float, optional): Percentage of data to use for peak extraction. Defaults to None.
+            min_dist (int, optional): Minimum distance between peaks. Defaults to None.
+
+        Returns:
+            tuple: A tuple containing the generated maps and peak indices.
         """
         data_io = DataIO()
         all_preprocessed_paths, _ = data_io.find_data(preprocessed_folder, extension)
