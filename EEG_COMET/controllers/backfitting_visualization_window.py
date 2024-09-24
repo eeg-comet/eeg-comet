ac84752042aaeea23802844d8ce2125753442040
aminkabir <kabir@sfu.ca>
2024-09-24 09:07:11 -0700

    Update backfitting_visualization_window.py

--- EEG_COMET/controllers/backfitting_visualization_window.py
+++ EEG_COMET/controllers/backfitting_visualization_window.py
@@ -1,4 +1,3 @@
-
 import os.path
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog, QFileDialog, QSizePolicy
@@ -12,9 +11,12 @@ from data_utils.data_io import DataIO
 from backfitting_utils.segmentation_io import SegmentationIO
 from gui_utils.set_widgets_status import set_widgets_status
 
-# TODO: organize functions and add descriptions
+
 class BackfittingVisualizationWindow(QDialog):
     def __init__(self, context, parent=None):
+        """
+        Initialize the BackfittingVisualizationWindow.
+        """
         super().__init__(parent)
 
         # Initialize variables
@@ -25,37 +27,47 @@ class BackfittingVisualizationWindow(QDialog):
         self._initialize_ui()
 
     def _load_ui(self, context):
-        # Load UI from the specified file
+        """
+        Load the UI layout from the specified .ui file.
+        """
         basepath = os.path.dirname(__file__)
         self.ui = uic.loadUi(context.get_resource("BackfittingVisualizationWindow.ui"), self)
 
     def _initialize_ui(self):
-        # Set up the main UI components
+        """
+        Initialize and set up the main UI components, including the plot canvas and signal-slot connections.
+        """
+        # Set window properties
         self.ui.setWindowTitle("Visualization of the backfitted microstates")
-        # Set window flags to include the maximize button
         self.setWindowFlags(self.windowFlags() | Qt.WindowMaximizeButtonHint)
 
+        # Initialize matplotlib Figure and Canvas
         self.figure = Figure(tight_layout=True)
         self.canvas = FigureCanvasQTAgg(self.figure)
         self.canvas.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.canvas.setMinimumHeight(100)
         self.ui.Figure_Layout.addWidget(self.canvas)
 
-        # Connect signals to slots
+        # Connect UI signals to their respective slots
         self.ui.show_backfitting_button.clicked.connect(self.show_backfitting)
         self.ui.export_backfitting_image_button.clicked.connect(self.export_plot)
         self.ui.figure_settings_checkbox.clicked.connect(self.backfitting_visualization_controller)
 
-        # Initialize UI state
+        # Initialize UI state based on default settings
         self.backfitting_visualization_controller()
 
     def backfitting_visualization_controller(self):
-        # Control the visibility and enable/disable state of UI widgets based on conditions
+        """
+        Control the visibility and enable/disable state of UI widgets based on the current data type
+        and user-selected settings.
+        """
+        # Widgets related to epoched data
         epoched_data_widgets = [
             self.ui.num_trials_label,
             self.ui.num_trials_spinbox
         ]
 
+        # Widgets related to figure settings
         figure_settings_widgets = [
             self.ui.font_size_label,
             self.ui.font_size_input,
@@ -65,6 +77,7 @@ class BackfittingVisualizationWindow(QDialog):
             self.ui.colormap_combobox,
         ]
 
+        # Enable or disable epoched data widgets based on datatype
         if self.datatype == 'epoched':
             set_widgets_status(epoched_data_widgets, mode='enable')
             set_widgets_status(epoched_data_widgets, mode='show')
@@ -76,19 +89,75 @@ class BackfittingVisualizationWindow(QDialog):
             set_widgets_status(epoched_data_widgets, mode='disable')
             set_widgets_status(epoched_data_widgets, mode='hide')
 
+        # Show or hide figure settings widgets based on checkbox state
         if self.ui.figure_settings_checkbox.isChecked():
             set_widgets_status(figure_settings_widgets, mode='show')
         else:
             set_widgets_status(figure_settings_widgets, mode='hide')
 
+    def _load_data_and_segmentation(self, selected_file_name):
+        """
+        Load EEG data and corresponding segmentation data from files.
+        """
+        # Initialize DataIO to load EEG data
+        data_io = DataIO()
+        eeg_dir, _ = data_io.find_data(
+            input_folder=self.preprocessed_data_path,
+            extension=self.extension,
+            pattern=f"*{selected_file_name}*"
+        )
+        eeg = data_io.load_eeg(eeg_path=eeg_dir[0], datatype=self.datatype)
+        eeg_data = eeg.get_data()
+        eeg_times = eeg.times * 1000  # Convert to milliseconds
+
+        # Process EEG data based on datatype
+        data_to_use = self._get_data_to_use(eeg_data)
+
+        # Load segmentation data
+        segmentation_io = SegmentationIO()
+        segmentation_filename = f"{selected_file_name}{self.export_format}"
+        segmentation_path = os.path.join(self.segmentation_path, segmentation_filename)
+        segmentation_data = segmentation_io.load_segmentation(segmentation_path, import_format=self.export_format)
+
+        return data_to_use, eeg_times, segmentation_data
+
+    def _get_data_to_use(self, eeg_data):
+        """
+        Determine the EEG data to use for plotting based on the current datatype.
+        """
+        if self.datatype == 'epoched':
+            trial = self.ui.num_trials_spinbox.value()
+            return np.std(eeg_data[trial - 1, :, :], axis=0)
+        return np.std(eeg_data, axis=0)
+
+    def _get_time_range(self):
+        """
+        Retrieve the time range for plotting from the UI inputs.
+        """
+        time_min = int(self.ui.xlim_min_input.text())
+        time_max = int(self.ui.xlim_max_input.text())
+        return time_min, time_max
+
+    def _get_plot_parameters(self):
+        """
+        Retrieve plot customization parameters from the UI inputs.
+        """
+        fontsize = int(self.ui.font_size_input.text())
+        labelsize = int(self.ui.label_size_input.text())
+        colormap = self.ui.colormap_combobox.currentText()
+        return fontsize, labelsize, colormap
+
     def show_backfitting(self):
-        # Handle the logic for displaying backfitting data
+        """
+        Handle the logic for displaying backfitting data by loading the selected EEG and segmentation data,
+        retrieving plot parameters, and rendering the plot.
+        """
         selected_file_name = self.ui.eeg_filenames_combobox.currentText()
 
-        # Load EEG data and segmentation data
+        # Load EEG and segmentation data
         eeg_data, eeg_times, segmentation_data = self._load_data_and_segmentation(selected_file_name)
 
-        # Get time range and other parameters
+        # Get plot parameters
         time_min, time_max = self._get_time_range()
         if self.datatype == 'epoched':
             trial = self.ui.num_trials_spinbox.value()
@@ -100,78 +169,47 @@ class BackfittingVisualizationWindow(QDialog):
         # Plot the data
         self._plot_data(eeg_times, eeg_data, data2plot, time_min, time_max, fontsize, labelsize, colormap)
 
-    def _load_data_and_segmentation(self, selected_file_name):
-        # Load EEG data and segmentation data from files
-        data_io = DataIO()
-        eeg_dir, _ = data_io.find_data(
-            input_folder=self.preprocessed_data_path,
-            extension=self.extension,
-            pattern=f"*{selected_file_name}*"
-        )
-        eeg = data_io.load_eeg(eeg_path=eeg_dir[0], datatype=self.datatype)
-        eeg_data = eeg.get_data()
-        eeg_times = eeg.times * 1000
-
-        data_to_use = self._get_data_to_use(eeg_data)
-
-        segmentation_io = SegmentationIO()
-        segmentation_filename = f"{selected_file_name}{self.export_format}"
-        segmentation_path = os.path.join(self.segmentation_path, segmentation_filename)
-        segmentation_data = segmentation_io.load_segmentation(segmentation_path, import_format=self.export_format)
-
-        return data_to_use, eeg_times, segmentation_data
-
-    def _get_plot_parameters(self):
-        # Get plot parameters from UI components
-        fontsize = int(self.ui.font_size_input.text())
-        labelsize = int(self.ui.label_size_input.text())
-        colormap = self.ui.colormap_combobox.currentText()
-        return fontsize, labelsize, colormap
-
-    def _get_data_to_use(self, eeg_data):
-        # Get the data to use based on the datatype
-        if self.datatype == 'epoched':
-            trial = self.ui.num_trials_spinbox.value()
-            return np.std(eeg_data[trial - 1, :, :], axis=0)
-        return np.std(eeg_data, axis=0)
-
-    def _get_time_range(self):
-        # Get the time range from UI components
-        time_min = int(self.ui.xlim_min_input.text())
-        time_max = int(self.ui.xlim_max_input.text())
-        return time_min, time_max
-
     def _plot_data(self, eeg_times, data_to_use, segmentation_data, time_min, time_max, fontsize, labelsize, colormap):
-        # Plot the data using matplotlib
+        """
+        Plot the EEG data along with segmentation overlays using matplotlib.
+        """
+        # Determine indices for the specified time range
         xmin = np.argmin(np.abs(eeg_times - time_min))
         xmax = np.argmin(np.abs(eeg_times - time_max))
 
-        # Limit the data and times to the desired time range
+        # Slice data and time arrays to the desired range
         times_to_use = eeg_times[xmin:xmax]
         data_to_use = data_to_use[xmin:xmax]
         segmentation_to_use = segmentation_data[xmin:xmax]
 
+        # Clear the previous plot
         ax = self.canvas.figure.gca()
         ax.clear()
 
-        # Generate the plot
+        # Prepare legend and color mapping
         legend_elements, color_map = self._prepare_legend_and_colors(segmentation_to_use, colormap)
+
+        # Fill the plot with data and segmentation overlays
         self._fill_plot(ax, times_to_use, data_to_use, segmentation_to_use, color_map)
 
+        # Customize plot appearance
         ax.set_xlabel('Time (ms)', fontsize=fontsize)
         ax.set_ylabel('Potential (μV)', fontsize=fontsize)
         ax.tick_params(axis='both', which='major', labelsize=labelsize)
         ax.set_xlim([time_min, time_max])
         ax.legend(handles=legend_elements, loc='upper right', fontsize=fontsize)
 
+        # Render the plot on the canvas
         self.canvas.draw()
 
     @staticmethod
     def _prepare_legend_and_colors(segmentation_data, colormap):
-        # Prepare legend and colors for the plot
+        """
+        Prepare legend elements and a color map based on segmentation labels.
+        """
         unique_labels = sorted(set(segmentation_data))
         cm = plt.get_cmap(colormap)
-        unique_colors = [cm(1. * i / len(unique_labels)) for i in range(len(unique_labels))]
+        unique_colors = [cm(1.0 * i / len(unique_labels)) for i in range(len(unique_labels))]
         color_map = dict(zip(unique_labels, unique_colors))
         legend_elements = [Patch(facecolor=color, edgecolor='none', label=label) for
                            label, color in zip(unique_labels, unique_colors)]
@@ -179,7 +217,9 @@ class BackfittingVisualizationWindow(QDialog):
 
     @staticmethod
     def _fill_plot(ax, times, data, segmentation, color_map):
-        # Fill the plot with data and segmentation information
+        """
+        Fill the plot with EEG data and color-coded segmentation regions.
+        """
         prev_index = 0
         for idx, label in enumerate(segmentation[1:], start=1):
             if label != segmentation[idx - 1]:
@@ -194,17 +234,25 @@ class BackfittingVisualizationWindow(QDialog):
         ax.fill_between(times[prev_index:], data[prev_index:], color=segment_color)
 
     def export_plot(self):
-        # Export the current plot to a file
+        """
+        Open a file dialog to export the current plot as an image file (PNG, JPG, etc.).
+        """
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getSaveFileName(self, "Export Plot", "",
-                                                   "PNG Files (*.png);;JPG Files (*.jpg);;All Files (*)",
-                                                   options=options)
+        file_name, _ = QFileDialog.getSaveFileName(
+            self,
+            "Export Plot",
+            "",
+            "PNG Files (*.png);;JPG Files (*.jpg);;All Files (*)",
+            options=options
+        )
         if file_name:
             self._save_plot(file_name)
 
     def _save_plot(self, file_name):
-        # Save the current plot to the specified file
+        """
+        Save the current plot to the specified file.
+        """
         extension = os.path.splitext(file_name)[-1].lower()
         if not extension:
             file_name += '.png'
